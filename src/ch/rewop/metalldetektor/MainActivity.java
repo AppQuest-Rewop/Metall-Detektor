08c2a7bf283e4757e2ddc14f196767968a671600
dev <dev@EvrenMint>
2013-10-02 23:08:37 +0200

    Alle Änderungen gemacht und Logbuch eingebaut.

--- src/ch/rewop/metalldetektor/MainActivity.java
+++ src/ch/rewop/metalldetektor/MainActivity.java
@@ -6,10 +6,19 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements SensorEventListener {
 	private SensorManager sensorManager;
@@ -18,6 +27,37 @@ public class MainActivity extends Activity implements SensorEventListener {
 	private TextView anzeigeFeld;
 	private ProgressBar anzeigeBar;
 	
+	private static final int SCAN_QR_CODE_REQUEST_CODE = 0;
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuItem menuItem = menu.add("In Logbuch eintragen");
+		menuItem.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+
+			@Override
+			public boolean onMenuItemClick(MenuItem item) {
+				Intent intent = new Intent(
+						"com.google.zxing.client.android.SCAN");
+				intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+				startActivityForResult(intent, SCAN_QR_CODE_REQUEST_CODE);
+				return false;
+			}
+		});
+
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (requestCode == SCAN_QR_CODE_REQUEST_CODE) {
+			if (resultCode == RESULT_OK) {
+				String qrCode = intent.getStringExtra("SCAN_RESULT");
+				Toast.makeText(this, qrCode, Toast.LENGTH_LONG).show();
+				sendlog(qrCode);
+			}
+		}
+	}
+	
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,20 +65,16 @@ public class MainActivity extends Activity implements SensorEventListener {
         setContentView(R.layout.activity_main);
         
         anzeigeFeld = (TextView) findViewById(R.id.sensorData);
-        anzeigeBar = (ProgressBar) findViewById(R.id.progressBar1);
+        anzeigeBar = (ProgressBar) findViewById(R.id.sensorBar);
 
-        
+        try{
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         magnetSensor = sensorManager.getSensorList(Sensor.TYPE_MAGNETIC_FIELD).get(0);
-        
-        anzeigeBar.setMax(600);
-        System.out.println("" + magnetSensor.getMaximumRange());
-        //sensorManager.registerListener(this, magnetSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        
-
-      
-        //anzeigeFeld.setText(magnetSensor);
-        
+        }
+        catch(Exception e) {
+        	Log.d(SENSOR_SERVICE, e.getMessage());
+        	Toast.makeText(this, "No Magnetic-Field Sensor detected :0 - are you using an emulator?", Toast.LENGTH_LONG).show();
+        }        
     }
     
     protected void onPause(){
@@ -48,22 +84,13 @@ public class MainActivity extends Activity implements SensorEventListener {
     
     protected void onResume(){
     	super.onResume();
-    	
     	sensorManager.registerListener(this, magnetSensor, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
     }
 
 
 	@Override
 	public void onAccuracyChanged(Sensor arg0, int arg1) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 
@@ -71,7 +98,29 @@ public class MainActivity extends Activity implements SensorEventListener {
 	public void onSensorChanged(SensorEvent event) {
 		messwert = (int) Math.sqrt(event.values[0] * event.values[0] + event.values[1] * event.values[1] + event.values[2] * event.values[2]);
 		anzeigeFeld.setText("MagnetFeld: " + messwert + " mikroTesla");
-		anzeigeBar.setProgress((int) messwert);
+		anzeigeBar.setProgress(messwert);
+	}
+	
+	
+	//eintrag in logbuch
+	private void sendlog(String qrCode) {
+		Intent intent = new Intent("ch.appquest.intent.LOG");
+		 
+		if (getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY).isEmpty()) {
+		Toast.makeText(this, "Logbook App not Installed", Toast.LENGTH_LONG).show();
+		return;
+		}
+		
+		EditText codeText = (EditText) findViewById(R.id.codeEdit);
+		String code = codeText.getText().toString();
+		 
+		intent.putExtra("ch.appquest.taskname", "REWOP.Metall-Detektor");
+		intent.putExtra("ch.appquest.logmessage", qrCode + ": " + code);
+		Toast.makeText(this, qrCode + ": " + code, Toast.LENGTH_LONG).show();
+		
+		 
+		startActivity(intent);
+		
 	}
     
 }
